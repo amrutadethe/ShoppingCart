03cb1f9e38194bbf0cbe42ea0b2b69c75413f216
nitin.anchal <nitin.anchal@228dd485-7601-0010-9e0a-5d6d7f786c1d>
2020-12-22 10:55:54 +0000

    New version first commit

---
+++ Punchout.DAL/DALProduct.cs
@@ -0,0 +1,127 @@
+﻿using Punchout.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punchout.DAL
+{
+    public class DALProduct
+    {
+        MAS_CMLEntities objMASCMLEntities = new MAS_CMLEntities();
+
+        public List<CI_Item> GetProductList(string searchText, string ItemText, string siteId, string sitedesc)
+        {
+            //List<CI_Item> CI_Item = objMASCMLEntities.CI_Item.ToList();
+            //return CI_Item;
+            try
+            {
+                DataTable dt = new DataTable();
+                //SqlConnection con = new SqlConnection("Data Source=PUNE-LAPTOP-136;Initial Catalog=MAS_CML;User ID=sa;Password=pass123!@#");
+                SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=MAS_CML;User ID=sa;Password=pass123!@#");
+                SqlCommand cmd = new SqlCommand("select ItemCode,ItemCodeDesc,SalesUnitOfMeasure,Category1,Category2,StandardUnitPrice,UDF_HNW_CLASSIFICATION_CODE from CI_Item where StandardUnitPrice>0 and ItemCodeDesc LIKE @searchText and ItemCode Like @itemText and SUBSTRING([ItemCode], 1, 2) = 'HW' and SUBSTRING([ItemCode], 4, 4) LIKE  @Site ", con);
+                //cmd.Parameters.AddWithValue("@searchText",searchText);
+                //cmd.Parameters.AddWithValue("@itemText",ItemText);
+                //cmd.Parameters.AddWithValue("@Site", site);
+                cmd.Parameters.AddWithValue("@searchText", string.Format("%{0}%", searchText));
+                cmd.Parameters.AddWithValue("@itemText", string.Format("%{0}%", ItemText));
+                cmd.Parameters.AddWithValue("@Site", string.Format("%{0}%", siteId));
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                var allProductList = new List<CI_Item>();
+                if (dt.Rows.Count > 0)
+                {
+                    allProductList = dt.AsEnumerable().Select(rows => new CI_Item
+                    {
+                        ItemCode = Convert.ToString(rows["ItemCode"]),
+                        ItemCodeDesc = Convert.ToString(rows["ItemCodeDesc"]),
+                        SalesUnitOfMeasure = Convert.ToString(rows["SalesUnitOfMeasure"]),
+                        Category1 = Convert.ToString(rows["Category1"]),
+                        Category2 = Convert.ToString(rows["Category2"]),
+                        StandardUnitPrice = Convert.ToDecimal(rows["StandardUnitPrice"]),
+                        UDF_HNW_CLASSIFICATION_CODE = Convert.ToString(rows["UDF_HNW_CLASSIFICATION_CODE"]),
+                    }).ToList();
+                }
+                return allProductList;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public List<CI_Item> GetProductListByItemCode(string itemText, string site_code, string site_desc)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                //SqlConnection con = new SqlConnection("Data Source=PUNE-LAPTOP-136;Initial Catalog=MAS_CML;User ID=sa;Password=pass123!@#");
+                SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=MAS_CML;User ID=sa;Password=pass123!@#");
+                SqlCommand cmd = new SqlCommand("select ItemCode,ItemCodeDesc,SalesUnitOfMeasure,Category1,Category2,StandardUnitPrice,UDF_HNW_CLASSIFICATION_CODE from CI_Item where StandardUnitPrice>0 and ItemCode Like @itemText  and SUBSTRING([ItemCode], 1, 2) = 'HW' and SUBSTRING([ItemCode], 4, 4) LIKE  @Site ", con);
+                //cmd.Parameters.AddWithValue("@searchText",searchText);
+                //cmd.Parameters.AddWithValue("@itemText",ItemText);
+                //cmd.Parameters.AddWithValue("@Site", site);
+                //cmd.Parameters.AddWithValue("@searchText", string.Format("%{0}%", searchText));
+                cmd.Parameters.AddWithValue("@itemText", string.Format("%{0}%", itemText));
+                cmd.Parameters.AddWithValue("@Site", string.Format("%{0}%", site_code));
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                var allProductList = new List<CI_Item>();
+                if (dt.Rows.Count > 0)
+                {
+                    allProductList = dt.AsEnumerable().Select(rows => new CI_Item
+                    {
+                        ItemCode = Convert.ToString(rows["ItemCode"]),
+                        ItemCodeDesc = Convert.ToString(rows["ItemCodeDesc"]),
+                        SalesUnitOfMeasure = Convert.ToString(rows["SalesUnitOfMeasure"]),
+                        Category1 = Convert.ToString(rows["Category1"]),
+                        Category2 = Convert.ToString(rows["Category2"]),
+                        StandardUnitPrice = Convert.ToDecimal(rows["StandardUnitPrice"]),
+                        UDF_HNW_CLASSIFICATION_CODE = Convert.ToString(rows["UDF_HNW_CLASSIFICATION_CODE"]),
+                    }).ToList();
+                }
+                return allProductList;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public CI_Item GetProductDetails(string id)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=MAS_CML;User ID=sa;Password=pass123!@#");
+                SqlCommand cmd = new SqlCommand("SELECT i.ExtendedDescriptionText,c.ItemCode,c.ItemCodeDesc,c.Category1,c.Category2  FROM [MAS_CML].[dbo].[CI_ExtendedDescription] i, [MAS_CML].[dbo].[CI_Item] c WHERE i.ExtendedDescriptionKey = c.ExtendedDescriptionKey AND ItemCode Like @itemText", con);
+                cmd.Parameters.AddWithValue("@itemText", string.Format("%{0}%", id));
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                CI_Item ProductDetail = new CI_Item();
+                if (dt.Rows.Count > 0)
+                {
+                    ProductDetail.ExtendedDescriptionKey = Convert.ToString(dt.Rows[0]["ExtendedDescriptionText"]);
+                    ProductDetail.ItemCode = Convert.ToString(dt.Rows[0]["ItemCode"]);
+                    ProductDetail.ItemCodeDesc = Convert.ToString(dt.Rows[0]["ItemCodeDesc"]);
+                    ProductDetail.Category1 = Convert.ToString(dt.Rows[0]["Category1"]);
+                    ProductDetail.Category2 = Convert.ToString(dt.Rows[0]["Category2"]);
+                }
+                return ProductDetail;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Punchout.Web/Models/hw_sites.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Punchout.Web.Models
+{
+    public class hw_sites
+    {
+        public string site_code { get; set; }
+        public string site_desc { get; set; }
+        public List<hw_sites> siteinfo { get; set; }
+    }
+}
